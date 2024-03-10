aaf51cc681cd8c7b7ff8a549f0267d1a5aeaea1c
Vojtasus <162464487+Vojtasus@users.noreply.github.com>
2024-03-10 10:54:44 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,116 @@
+const nadpis = document.getElementsByTagName("h1") [0] ;
+nadpis.addEventListener("click", (e) => {
+    e.target.innerHTML = "WE ARE HERE TO HELP";
+})
+
+
+
+function sum(cislo1,cislo2){
+    const finalnicislo= cislo1>cislo2;
+}
+console.log(finalnícislo);
+sum(1.2);
+function vypisdatum(){
+    const datum=new date();
+const den=datum.getdate(); +1//tohle se vratí v roce (-1)
+const měsíc = datum getfullyear:(); tohle se vratí současný rok
+const zobrazdatum =´ ${´den´}/${´mesic´}/${´rok´}´
+const casovyelement=document.getElementById("cas");
+casovyelement.innerHTML=zobrazdatum
+}
+vypisdatum(2024);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
